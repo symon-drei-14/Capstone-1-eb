ad9323d62b5ae20484f063e7528799d0f4b1296c
Symon Alcedo <symonandrei.alcedo@letran.edu.ph>
2025-10-18 03:39:19 +0800

    fix error

--- include/handlers/get_driving_drivers.php
+++ include/handlers/get_driving_drivers.php
@@ -6,24 +6,24 @@ require 'dbhandler.php';
  * Get all drivers with pending status
  * @return array Array of drivers and their destinations
  */
-function getDrivingDrivers() {
-    global $conn;
+// function getDrivingDrivers() {
+//     global $conn;
     
-    $drivers = array();
+//     $drivers = array();
     
 
-    $query = "SELECT driver, destination FROM assign WHERE status = 'pending'";
-    $result = $conn->query($query);
+//     $query = "SELECT driver, destination FROM assign WHERE status = 'pending'";
+//     $result = $conn->query($query);
     
   
-    if ($result && $result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $drivers[] = $row;
-        }
-    }
+//     if ($result && $result->num_rows > 0) {
+//         while ($row = $result->fetch_assoc()) {
+//             $drivers[] = $row;
+//         }
+//     }
     
-    return $drivers;
-}
+//     return $drivers;
+// }
 
 function getOngoingDeliveriesCount() {
     global $conn;
